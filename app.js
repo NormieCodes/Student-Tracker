3b3463c8c0be95ed46d6ea59b6a1169c94abc744
NormieCodes <rishisagaram5@gmail.com>
2026-02-08 10:34:05 +0530

    saving workstate

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 // Access all anchor elements in the DOM
-const link = document.querySelectorAll("a");
+const links = document.querySelectorAll("a");
 const home = document.getElementById('home');
 const tasks = document.getElementById('task-manager');
 
 // add event listeners to anchor tags
-link.forEach(link => {
+links.forEach(link => {
  link.addEventListener("click", event => {
    const clickedLink = event.currentTarget;
-   const target = clickedLink.getAttribute("href");
-   
-    alert('it is working!'); 
+   const target = clickedLink.getAttribute("href").replace("#","");
+   showView(target);
+   history.pushState({view: target}, "", "#" + target);
+   history.replaceState({ view: "home" }, "", "#home");
+   alert('it is working!'); 
    console.log(clickedLink);
    console.log(target);
-    if (target === '#task-manager') {
-        home.classList.add('hidden');
-        tasks.classList.remove('hidden');
-    }
- });
 });
+});
+
+//back/forward navigation 
+window.addEventListener('popstate', event => {
+    console.log(event.state);
+    if (event.state && event.state.view) {
+        showView(event.view.state);
+    }
+});
+    
+function showView (id) {
+    const views = document.querySelectorAll('.view');
+    views.forEach(v => v.classList.add('hidden'));
+    document.getElementById(id).classList.remove('hidden');
+}
+

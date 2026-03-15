d191e872f1f7c64e910d6b717bbda05de9a82a62
NormieCodes <rishisagaram5@gmail.com>
2026-03-15 20:36:00 +0530

    traversal logic for task manager

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const li = document.querySelectorAll(".list-js");
-const main = document.querySelector("#main");
+const menuItems = document.querySelectorAll(".menu-item");
+const pages = document.querySelectorAll(".page");
+const menu = document.querySelector(".menu");
+const home = document.querySelector(".home");
 
-li.forEach((list) => {
-  list.addEventListener("click", function (event){
-    console.log(event.target.innerText);
-    main.classList.toggle("hidden");
-    main.parentElement.firstElementChild.classList.toggle("hidden");
+menuItems.forEach((item) => {
+  item.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    const sectionName = e.target.dataset.section;
+
+    console.log(sectionName);
+    menu.classList.add("hidden");
+    menu.previousElementSibling.classList.add("hidden");
+
+    pages.forEach((page) => {
+      page.classList.remove("active");
+    });
+
+    const activePage = document.querySelector(`.page[data-section="${sectionName}"]`);
+    activePage.classList.add("active");
   });
 })
+
+home.addEventListener("click", () => {
+  pages.forEach((page) => {
+    page.classList.remove("active");
+  });
+
+  menu.classList.remove("hidden");
+  menu.previousElementSibling.classList.remove("hidden");
+});
